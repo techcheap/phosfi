5bc92b6455c20cba045ad809406bcb6176fce303
techcheap <82733867+techcheap@users.noreply.github.com>
2021-10-27 23:27:07 +0200

    test

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-import * as THREE from 'three';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+const geometry1 = new THREE.TorusGeometry(10, 3, 30, 200);
+const material1 = new THREE.MeshBasicMaterial({color: 0xff0000});
+const torus = new THREE.Mesh( geometry1 , material1);
+scene.add( torus );
+torus.position.x = 3;
+
+camera.position.z = 50;
+
+const animate = function () {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+};
+
+animate();
