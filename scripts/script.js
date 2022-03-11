32edce75d2dd477c63f6ffb2cbc2fa2d8e7be51a
techcheap <82733867+techcheap@users.noreply.github.com>
2022-03-11 22:54:32 +0100

    camera1

--- scripts/script.js
+++ scripts/script.js
@@ -43,7 +43,7 @@ var firebaseRef = firebase.database();
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror:false, facingMode:"environment" });
 Instascan.Camera.getCameras().then(function (cameras) {
     if (cameras.length > 0) {
-        scanner.start(cameras[0]);
+        scanner.start(cameras[1]);
         document.getElementById("name").innerHTML = cameras.length;
     } else {
         console.error('No cameras found.');
