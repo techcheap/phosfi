f0e6423ebd94f89846b9989608adb436d3350ec3
techcheap <82733867+techcheap@users.noreply.github.com>
2022-03-12 11:34:14 +0100

    camera select

--- scripts/script.js
+++ scripts/script.js
@@ -31,6 +31,54 @@ if (hasGetUserMedia()) {
     alert("getUserMedia() is not supported by your browser");
 }
 
+var videoSelect = document.querySelector('select#videoSource');
+
+videoSelect.onchange = getStream;
+getStream().then(getDevices).then(gotDevices);
+
+function getDevices() {
+    // AFAICT in Safari this only gets default devices until gUM is called :/
+    return navigator.mediaDevices.enumerateDevices();
+}
+
+function gotDevices(deviceInfos) {
+    window.deviceInfos = deviceInfos; // make available to console
+    console.log('Available input and output devices:', deviceInfos);
+    for (const deviceInfo of deviceInfos) {
+        const option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+        if (deviceInfo.kind === 'videoinput') {
+            option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
+            videoSelect.appendChild(option);
+        }
+    }
+}
+
+function getStream() {
+    if (window.stream) {
+        window.stream.getTracks().forEach(track => {
+            track.stop();
+        });
+    }
+    const videoSource = videoSelect.value;
+    const constraints = {
+        video: { deviceId: videoSource ? { exact: videoSource } : undefined }
+    };
+    return navigator.mediaDevices.getUserMedia(constraints).
+        then(gotStream).catch(handleError);
+}
+
+function gotStream(stream) {
+    window.stream = stream; // make stream available to console
+    videoSelect.selectedIndex = [...videoSelect.options].
+        findIndex(option => option.text === stream.getVideoTracks()[0].label);
+    video.srcObject = stream;
+}
+
+function handleError(error) {
+    console.error('Error: ', error);
+}
+
 const constraints = {
     video: true,
 };
