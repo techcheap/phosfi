c4ac125a6047e3c85f08ea7d8ba7ca5e82ac9a21
techcheap <82733867+techcheap@users.noreply.github.com>
2022-03-12 19:10:56 +0100

    working face detection

---
+++ ireco.js
@@ -0,0 +1,51 @@
+//image recognition
+const myname = document.getElementById("name")
+const imageUpload = document.getElementById('imageUpload')
+
+Promise.all([
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+]).then(start)
+
+async function start() {
+    const container = document.createElement('div')
+    container.style.position = 'relative'
+    document.body.append(container)
+    const labeledFaceDescriptors = await loadLabeledImages()
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.8)
+    let image
+    let canvas
+    imageUpload.style.display="block";
+    imageUpload.addEventListener('change', async () => {
+        if (image) image.remove()
+        if (canvas) canvas.remove()
+        image = await faceapi.bufferToImage(imageUpload.files[0])
+        canvas = faceapi.createCanvasFromMedia(image)
+        const displaySize = { width: image.width, height: image.height }
+        faceapi.matchDimensions(canvas, displaySize)
+        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+        results.forEach((result, i) => {
+            const box = resizedDetections[i].detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+            drawBox.draw(canvas)
+            myname.innerHTML = result.toString()
+        })
+    })
+}
+
+function loadLabeledImages() {
+    const labels = ["Abraham Lincoln", "Barack Obama", "Bill Clinton", "Dwight Eisenhower", "Franklin Delano Roosevelt", "George Bush", "George Washington", "Harry Truman", "James Abraham Garfield", "Joe Biden", "John Fitzgerald Kennedy", "McKinley", "Richard Nixon", "Ronald Reagan", "Ulysse Grant", "Woodrow Wilson"]
+    return Promise.all(
+        labels.map(async label => {
+            const descriptions = []
+            const img = await faceapi.fetchImage(`labeled_images/${label}/1.jpg`)
+            const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+            descriptions.push(detections.descriptor)
+
+            return new faceapi.LabeledFaceDescriptors(label, descriptions)
+        })
+    )
+}
